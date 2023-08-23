8018cf4c41f24debf22a7e699430a9368e88cd25
Ruth Condori <elith.tati@gmail.com>
2023-08-23 10:16:41 -0300

    S3- 22/08

---
+++ condicionales.py
@@ -0,0 +1,49 @@
+#Semana 3  Martes - 22/08/2023
+
+print("Ingrese dia de la semana y Fecha.")
+fecha = input("Formato: dia,dd/mm  ")
+dia_semana = fecha.index(',')
+dia = fecha[0:dia_semana]
+dia= dia.lower()
+#print(dia)
+dd_fin= fecha.index('/')
+dd= int(fecha[dia_semana+1:dd_fin])
+#print(dd)
+mm=int( fecha[dd_fin+1:])
+#print(mm)
+
+if (dia!="lunes" and dia!="martes" and dia!="miercoles" and dia!="jueves" and dia!="viernes"):
+    print("Dia incorrecto, vuelva a ingresar la fecha")
+
+if(dd>31):
+  print("DD incorrecto, vuelva a ingresar la fecha")
+
+elif(mm>12):
+  print("MM incorrecto, vuelva a ingresar la fecha")
+else:
+  print("Fecha ingresada de forma correcta!")
+
+if(dia=="lunes" or dia=="martes" or dia=="miercoles"):
+  examen= input("¿Hubo examen? (si/no)   ")
+  if(examen=="si"):
+    alumn_aprob=int(input("Ingrese cantidad de alumnos aprobados: "))
+    alumn_desap=int(input("Ingrese cantidad de alumnos desaprobados: "))
+    alumn_total= alumn_aprob+alumn_desap
+    aprobados= int(alumn_aprob*100/alumn_total)
+    print(f"Prosentaje de aprob: {aprobados}%")
+elif(dia=="jueves"):
+  print("Los jueves hay practicas habladas")
+  asistencia= int(input("Ingrese la cantidad de alumnos que asisten: "))
+  total_al=int(47)
+  if(asistencia>(total_al/2)):
+    print("Asistió la mayoría")
+  else:
+    print("No asistio la mayoría")
+else:
+  if(dd==1 and (mm==1 or mm==7)):
+    print("Comienzo de nuevo ciclo")
+    cant_a=int(input("Ingrese la cantidad de alumnos nuevos: "))
+    arancel=int(input("ingrese el arancel: $"))
+    ing_total= arancel*cant_a
+    print("El ingreso total es $",ing_total)
+  
---
+++ ejercicios.py
@@ -0,0 +1,50 @@
+#Ejercicios complementarios
+
+#1 y 2
+numero1= 15;
+numero2= 1.5;
+print(f"N1= {numero1} y N2= {numero2}")
+#3
+suma = numero1 + numero2;
+resta = numero1 - numero2;
+multiplicacion = numero1 * numero2;
+division = numero1 / numero2;
+#4
+print(f"N1+N2={suma}, N1-N2={resta}, N1+N2={multiplicacion}, N1/N2={division}")
+#5 al 8
+nombre= "Ruth";
+precio= 509.99
+descuento= 0.1
+precio_final= precio - precio*descuento;
+print("El precio final es $ ", precio_final)
+#9 y 10
+cadena = "La vie est belle";
+lingitud= len(cadena);
+print(f"cadena= {cadena} y su longitud es {lingitud}")
+#11
+precio=199.18
+precio_entero= int(precio)
+print(f"El precio real es ${precio} y el precio entero es ${precio_entero}")
+#12
+nombre= "Victor "
+apellido= "Sosa"
+nombre_completo= nombre + apellido;
+print(f"Su nombre completo es {nombre_completo}")
+#13
+edad=25;
+edad+=5
+edad-=15
+#14
+altura= 1.57
+calculo= altura *4 /3
+print(f"Altura = {altura}, altura*4/3 ={calculo}")
+#15
+nombre_mayus= "RUTH"
+print(nombre_mayus)
+nombre_minus=nombre_mayus.lower()
+print(nombre_minus)
+#16
+nom_may_min= nombre_mayus.capitalize()
+print(nom_may_min)
+
+
