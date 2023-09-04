ffeef2f12df4fd7da7c7b3679fbef80bb68e8c87
Ruth Condori <elith.tati@gmail.com>
2023-09-04 18:25:45 -0300

    Update tp3.py

--- tp3.py
+++ tp3.py
@@ -61,19 +61,104 @@ while password!="strong":
 print("Contraseña correcta!")
 
 #Ejercicio 10
+num = int(input("Ingrese un número entero positivo: "))
+cont= 0
+if  num>1:
+  for i in range(2, int(num/2)+1):
+    #print(i)
+    if num%i==0:
+      cont+=1
+if cont>=1 or num<=1:
+  print("No es primpo")
+else:
+  print("Es primo")
 
 #Ejercicio 11
+word=input("Ingrese una palabra: ")
+drow=""
+for i in range(len(word)-1,-1,-1):
+  drow+=word[i]
+print(drow)
 
 #Ejercicio 12
-
+print("Escriba una frase:")
+sent=input().lower()
+print("Digame una letra y le diré cuantas veces aparece en la frase.")
+lett=input("Letra: ").lower()
+time=0
+for i in range(len(sent)):
+  if sent[i]==lett:
+    time+=1
+print(f"La letra {lett} aparece: {time}")
+  
 #Ejercicio 13
+sent= input("Escribe lo que quieras: ")
+array=[]
+cont=0
+while sent!="salir":
+  array.append(sent)
+  sent= input("Escribe lo que quieras: ")
+  cont+=1
+for i in range(cont):
+ print(array[i])
 
 #Ejercicio 14
+a,b= int(input("Ingrese un numero: ")),int(input("Ingrese otro número : "))
+even=[]
+odd=[]
+  #ran=range(b,a+1) if a>b else ran=range(a,b+1)
+if a>b:
+  ran=range(b,a+1)
+else:
+  ran=range(a,b+1)
+for i in ran:
+ # print(i)
+  if i%2==0:
+    even.append(i)
+  else:
+    odd.append(i)
+print(f"Los números pares son: {even}. Los números impares son: {odd}")
 
 #Ejercicio 15
+num= int(input("Ingrese un número mayor a cero y le doy sus divisores: "))
+print("Sus divisores son:")
+for i in range(1,num+1):
+  if num%i==0:
+    print(i)
 
 #Ejercicio 16
+amount=int(input("¿Cuantos números desea ingresar? "))
+neg=0
+for i in range(1,amount+1):
+  num= int(input(f"Número{i}°: "))
+  if num<0:
+    neg+=1
+print(f"Introdujiste {neg} números negativos")
+
 #Ejercicio 17
+phrase=input("Ingresa una frase: ").lower()
+vowel="aeiou"
+print("Las vocales que aparecen: ")
+for j in range(0,len(vowel)):
+    if phrase.find(vowel[j])>-1:
+      print(vowel[j])  
+  
+
 #Ejercicio 18
+
+
 #Ejercicio 19
+
+
 #Ejercicio 20
+
+
+#Ejercicio 21
+
+
+#Ejercicio 22
+
+
+#Ejercicio 23
+#Ejercicio 24
+#Ejercicio 25
