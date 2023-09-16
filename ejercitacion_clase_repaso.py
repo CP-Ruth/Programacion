68276b11d255842d22c5b75355569f75398b7937
Ruth Condori <elith.tati@gmail.com>
2023-09-16 13:22:33 -0300

    ejercitacion

--- ejercitacion_clase_repaso.py
+++ ejercitacion_clase_repaso.py
@@ -6,18 +6,30 @@ for i in range(6):
     print(f"(cod:{i+1}).{list_article[i]} ${list_price[i]}")
 #Empezamos con el pedido
 name= input("¿Quién realiza el pedido?\n")
+print(f"{name}, empecemos con tu pedido!")
 new_list=[]
+arti_amount=[]
 amount_price=[]
 while name!="":
-    code=int(input("¿Qué artículo deseas comprar? Ingresa su codigo.\n"))
+    code=int(input("¿Qué artículo deseas comprar? (Ingresa su codigo): "))
+    while code<1 or code>6:
+        print("Número no es valido")
+        code=int(input("¿Qué artículo deseas comprar? (Ingresa su codigo): "))
+        if code>0 and code<7:
+            print(code)
+            break
     new_list.append(list_article[code-1])
-    amount=int(input("Cantidad a comprar de este artículo:\n"))
+    amount=int(input("Cantidad a comprar de este artículo: "))
+    arti_amount.append(amount)
     amount_price.append(list_price[code-1]*amount)
     
-    going=input("¿Desea agregar otro artículo? si/no \n").lower()
-    if going!="si":
-        break
+    going=input("Si ya no deseas agregar otro articulo ingresa 'N' \n").lower()
+    if going=="no":
+        break 
+#Detalle del pedido
+
 print(new_list)
+print(arti_amount)
 print(amount_price)
 
 
