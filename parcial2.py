073f1d889567f2a85d6812d50257a2abd93bf282
Ruth Condori <elith.tati@gmail.com>
2023-11-03 20:14:57 -0300

    Parcial 2
    Magneto, no me agradas -.-

--- parcial2.py
+++ parcial2.py
@@ -1,30 +1,47 @@
 from parcial2_funciones import *
 
-
+#Mensaje de inicio
+print("Saludos. \nBienvenido al testeo de ADN.\nVamos a hacer solo una pequeña prueba de tu ADN")
+moment = input("¡No te preocupes, no te vamos pinchar, no es necesaria tu sangre! (aún)\nPara continuar presiona enter.")
 
 while True:
-    # 1 Armamos la cadena de ADN 
-    adn = []
+
+    # 1 Armamos la cadena de ADN, es una lista de Strings.
+    adn = []   
+
+    # Armammos los strings y luego los incorporamos a la lista "adn"
     for i in range(6):
-        adnrow=""
+        adnrow=""     # String // fila de la matriz
+
         while True:
-
             letter = input("Ingresa una letra de - A,T,C,G - : ").upper()
-            #si retorna True:
+            
+            # La funcion "it_on" valida que el dato ingresado sea una de las letras "A,T,C,G "
+            # Si es así entonces retorna true e ingresa a la condición.
             if(it_on(letter)):
-                adnrow +=letter
+                adnrow += letter  # Concatena la letra ingresada al String
+            
+            # Solo si el largo del String "adnrow" es igual a 6 ingresa a la condición, si no continua con el bucle para completar el string hasta que cumpla la condición. 
             if(len(adnrow) == 6):
-                adn.append(adnrow)
-                space_betwen_string(adnrow) # Imprimomos la linea de 6 letras que ingresamos
+                adn.append(adnrow)  # Añadimos el string a la lista.
+                space_betwen_string(adnrow) # Imprimomos la linea de 6 letras que ingresamos con espacio entre las letras. Es solo un detalle visual.(No modifica al String "adnrow")
                 break    
-    space_betwen_matrix(adn) # Imprimimos la matriz que se armó.
-    is_mutant(adn)
+    
+    # Una vez que se haya completado el bucle for, que quiere decir que se llenó la matriz 6x6. Imprimimos la matriz que completamos y hacemos que pase por la prueba.
+    space_betwen_matrix(adn) # Imprimimos la matriz que se armó. (Nuevamente un detalle visual)
 
+    #print(adn)  Corroborar que adn sigue siendo una lista de string con el formato:  [‘ATGCGA’,’CAGTGC’,’TTATGT’,’AGAAGG’,’CCCCTA’,’TCACTG’]
+
+    is_mutant(adn) # Es la prueba para identificar si el adn ingresado es el de un mutante.
+
+    # Cuando temrina la prueba tenemos la opción de hacer o iniciar una nueva prueba. Si no se requiere entonces solo indicamos que "no". 
     while True:
         moment = input("¿Deseas analizar otro ADN? (si/no)\n").upper()
         if(moment == "SI" or moment == "NO"):
             break
     if(moment == "NO"):
         break
+
+# Mensaje saludo.
 print("Gracias por ayudar a Margento a buscar Mutantes. Adios.")
 
--- parcial2_funciones.py
+++ parcial2_funciones.py
@@ -1,20 +1,22 @@
-from colorama import init, Fore
+from colorama import init, Fore #importacion para usar colores :D 
 init(autoreset=True)  # Inicializa colorama
 
-#Validar que las letra ingresada sea A,T,C,G
+# Validar que las letra ingresada sea A,T,C,G
 def it_on(letra):
-    letter = ["A","T","C","G"]
-    if (letra in letter):
+    letter = ["A","T","C","G"]  # Lista de String con las letras permitidas
+    
+    # Si la letra ingresada está en la lista de Strings retorna true, si no devuelve el mensaje de que el valor no correspode y retorna false.
+    if (letra in letter):  
         return True
     else:
         print("El valor ingresado es incorrecto.")
         return False
-    
-#Imprimir los string con espacio intermedio SOLO TOMA LA FILA - FILA
-# option= fila entonces me imprime la fila cuando esta completa
-# option= matriz entonces me imprime la matriz completa
+
+# Imprimir los string con espacio intermedio SOLO TOMA LA FILA - FILA
 def space_betwen_string(element):
     new_string= ""
+
+    # Recorremos el string, y por cada vuelta agregamos un espacio más la letra del string a un nuevo string que luego imprimiremos.
     for i in range(len(element)):
         new_string += " "+element[i]
     print(new_string)
@@ -23,88 +25,97 @@ def space_betwen_string(element):
 def space_betwen_matrix(matrix):
 
     print("---------------------------------\n\nTu ADN matiz:")
-    for j in matrix:
+    # Recorremos la matrix por cada elemento(en este caso cada string). Luego usamos la función "space_betwen_string" para imprimir cada fila de la matriz con espacio intermedio.
+    for j in matrix: 
         space_betwen_string(j)
     print("---------------------------------")
 
 init(autoreset=True)  # Inicializa colorama
 
-# Pintar 
+# Pintar solo aquellos valores de la matriz que se especifican en las coordenadas ingresadas.
+
 def paint_letter(matrix1, coordenadas):
     
-    matrix = []
+    matrix = []  # Nueva lista(de listas) para especificar los elementos que vamos a pintar.
     
     for i in range(6):
-        row_matrix = []
+        row_matrix = []  # Acá vamos a armar una lista de string por cada letra. 
         for j in range(6):
-            row_matrix.append( matrix1[i][j])
+            row_matrix.append( matrix1[i][j]) # Acá añadimos por sepasado cada letra del string de la matriz "matrix1".
+            
+            # Acá corroboramos si el elemento que ingresamos, si posición es igual a alguna de las coordenadas ingresadas.
+            # Coordenadas es una lista de tuplas.
             for row, column in coordenadas:
+
+                # Si la posición que se recorre es igual a una de las coordenadas entonces se reemplza ese valor por el codigo para pintarlo más la letra que había.
                 if (row == i and column == j):
                     row_matrix[j] = Fore.YELLOW + matrix1[i][j]
         
-        matrix.append(row_matrix)
+        matrix.append(row_matrix)  # Una vez que completamos la fila, la añadimos a la matriz que vamos a imprimir.
     
     print("---------------------------------\nTu adn es: ")
     
+    # Para imprimir la matriz con colores tengo que especificar su posición/ imprimir elemento por elemento.
     for i in range(6):
         print(matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],matrix[i][4],matrix[i][5])
     print("---------------------------------")
 
-#Funcion para corroborar si es mutante
+# Funcion para corroborar si es mutante
 def is_mutant(matrix):
-    stop = False
+
     # Primero recorre cada fila
     for j in range(6):
         for i in range(3):
+            # Por cada fila recorro los primeros cuatro valores corroborando si esos 4 valores son iguales, si no es así se corre un lugar a la derecha corroborando esos 4 lugares. Dos veces se corre a la derecha. Luego de no encontrar coincidencias baja una fila y repite procedimiento.
             if (matrix[j][i]==matrix[j][i+1] ==matrix[j][i+2] ==matrix[j][i+3]):
+                # En caso que esas 4 posiciones sean iguales en valor, se toman esas posiciones para guardarlas cada una en una tupla como coordenadas para luego pasarlas en "paint_letter" junto con la matriz "adn" para imprimir con color.
                 coordinate=[(j,i),(j,i+1),(j,i+2),(j,i+3)]
                 print(Fore.GREEN +" ¡¡¡¡M U T A N T E !!!! == ")
                 paint_letter(matrix, coordinate)
-                stop = True
-                break
-        if(stop):
-            break
+                return True #
+
     # Segundo recorre cada columna
     for c in range(len(matrix)):
         for r in range(3):
+            #Es un procedimiento parecido al enterios solo que ahora verificamos los valores de manera vertical o por columna. Por lo que ahora en vez de ir a la derecha, bajamos 2 lugares. 
             if(matrix[r][c] == matrix[r+1][c] == matrix[r+2][c] == matrix[r+3][c]):
                 coordinate=[(r,c),(r+1,c),(r+2,c),(r+3,c)]
                 print(Fore.GREEN +" ¡¡¡¡M U T A N T E !!!! || ")
                 paint_letter(matrix,coordinate)
-                stop =  True
-                break
-        if(stop):
-            break
+                return True
+
     # Tercero diagonales \\
     "matrix[i][j] == matrix[i+1][j+1]"
     for i in range(3):
         for j in range(3):
+            # En el caso de las diagonales, la matriz se recorre de izquierda a derecha, de arriba a abajo. Se toman las primeras 4 filas y y se recorren las 3 primeras opciones de diagonales(de izquierda a derecha).
+            # Si  no hay coincidencias bajamos una fila y tomamos el valor de las 4 filas siguiente empezando por esa nueva (cambio del valor de i por el for) y se repite procedimiento de las diagonales de izquierda a derecha.
             if(matrix[i][j] == matrix[i+1][j+1] == matrix[i+2][j+2] == matrix[i+3][j+3]):
                 coordinate = [(i,j),(i+1,j+1),(i+2,j+2),(i+3,j+3)]
                 print(Fore.GREEN +" ¡¡¡¡M U T A N T E !!!! \\ ")
                 paint_letter(matrix,coordinate)
-                stop =  True
-                break
-        if(stop):
-            break
+                return True
+
     # Cuarto diagonales //
     for i in range(5,2,-1):
         for j in range(3):
+            # Para estas diagonales tenemos el mismo procedimiento solo que esta vez empezamos desde abajo. Es decir iniciamos el For desde la posicion 5 más los 3 valores anteriores a partir de 5 (filas: 5,4,3,2). Seguimos corroborando coincidencias de sus valores de izquierca a derecha.
             if(matrix[i][j] == matrix[i-1][j+1] == matrix[i-2][j+2] == matrix[i-3][j+3]):
                 coordinate = [(i,j),(i-1,j+1),(i-2,j+2),(i-3,j+3)]
                 print(Fore.GREEN +" ¡¡¡¡M U T A N T E !!!! // ")
                 paint_letter(matrix, coordinate)
-                stop =  True
-                break
-        if(stop):
-            break
-    if(stop == False):
-        print(Fore.RED + "No es mutante")
+                return True
+
+    return False, print(Fore.RED + "No es mutante")
 
 
 """# Ejemplo
-lolo =["AAAATC","ATGCCC","TTAGTT","ATCGTT","ACTGGC","ATTGGC"] #//
+lolo =["ATAATC","AGGCCC","GTAATT","ATCATT","ACTGCC","ATTGGC"]  #No mutante
+#lolo =["ATAATC","ATGCCC","GTAGTT","ATCGTT","ACTGGC","ATTGGC"]  #Mutante
+#lolo =["ATAATC","AGGCCC","GTAGTT","ATCGTT","ACTGGC","ATTGGC"]  #Mutante
+#lolo =["ATAATC","AGGCCC","GTCGTT","ATCCTT","ACTGCC","ATTGGC"]  #Mutante
+#lolo =["ATAATC","AGGCCC","GTCCTT","ATCATT","ACTGCC","ATTGGC"]  #Mutante
 space_betwen_matrix(lolo)
-is_mutant(lolo)
-"""
+is_mutant(lolo)"""
+
 
